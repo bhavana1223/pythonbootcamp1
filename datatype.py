8321e9cc2c1f296ae39914670e069a7ca8c091ed
bhavana1223 <21d21a1276.bhavana@gmail.com>
2024-07-23 21:54:22 +0530

    day1

---
+++ datatype.py
@@ -0,0 +1,78 @@
+python
+.programming lang
+.ml/dl
+.high level
+.oops
+DATA TYPES                                        
+1.integer
+2.float
+charcter-we wont take it
+3.string-by input data it will take default
+4.boolean
+5.sets
+6.tuples
+7.list
+8.dictionaries
+
+1.how to print a statement:we use print("information")
+ex:print("hello world")
+.by default python prints in a new(or)nextline
+.printing format
+.print stmt
+
+2.hello your name
+.to store any name we need a variable
+.var1="python"
+.print("good morning",var1)
+.o/p=good morning python
+
+3.taking i/p dynamically
+var assign from right-left
+name=input("enter your name\n")
+print(name)
+\n we uses for newline
+
+4.fstring==formatter stirng
+ex;age=50
+name="bhavana"
+required o/p:hello bhavana 
+hello {name} you are{age} year old
+
+program:age=input("enter your age")
+name=input("enter your name")
+print(f"hell0 {name} you are {age} year old")
+
+5.print your marks ,sum and avg with your name
+name=input("enter your name")
+sub1=input("enter your marks")
+sub2=input("enter your marks")
+sub3=input("enter your marks")
+sub4=input("enter your marks")
+ int sum=sub1+sub2+sub+sub4
+ avg=sum/4
+print(f"hello {name} your are {sum} and avg{sum/4}")
+
+6. name=input("hello")
+print(name,end=" ")  addtional space //to get desire o/p
+
+//CONDITIONAL STATEMENT-to check the conditions  
+1.IF:these are similar of try
+2.elif-catch
+3.Else-throw
+//program for driving license elgibiltiy 
+>>> age=int(input())
+20
+>>> if age>18:
+...    print("eligible")
+... else:
+...    print("not eligible")
+...
+eligible
+  
+BASIC OF OPERATIONS
+1.unary operator:single 
+2.arthametic ,logical,bitwise,assignment,ternary,relational 
+3.module:to reduce the input
+
+
+
